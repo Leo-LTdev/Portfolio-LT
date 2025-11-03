8f3bb6d05ff5dd01fe6c174e2e93e3fc7165d0f5
Stiximir <84101101+Stiximir@users.noreply.github.com>
2025-11-03 19:31:38 +0100

    Delete old css template and rework of app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,43 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="Site en cours de réalisation" />
-
+      <HelloWorld msg="Site en réalisation" />
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
-
 </template>
 
 <style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
 
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
 </style>
