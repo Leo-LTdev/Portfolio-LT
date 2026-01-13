836f36bc983dc3d1f8d49105780923635e09f991
Stiximir <stiximir100@gmail.com>
2026-01-13 17:06:14 +0100

    small fix for FCP

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -31,14 +31,14 @@ function handleClick() {
 
 <style scoped>
 .welcome--fade {
-  opacity: 0;
+  opacity: 0.1;
   transform: translateY(20px);
   animation: fadeIn 0.8s ease-out forwards;
 }
 
 @keyframes fadeIn {
   from {
-    opacity: 0;
+    opacity: 0.1;
     transform: translateY(20px);
   }
   to {
